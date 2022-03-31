ed61da263ada425353df1fc90c68d0add46ed048
Hrvoje Zec <hrvoje.zec99@gmail.com>
2022-03-31 16:28:28 +0200

    first commit

---
+++ backend/database.js
@@ -0,0 +1,23 @@
+// In-memory databse
+const clubs = [
+    { "position": 1, "image": "/images/manchCity.png", "name": "Manchester City", "played": "29", "won": "22", "drawn": "4", "lose": "3", "gd": "+50" },
+    { "position": 2, "name": "Liverpool", "played": "29", "won": "21", "drawn": "6", "lose": "2", "gd": 55, "points": "69" },
+    { "position": "3.", "name": "Chelsea", "played": "28", "won": "17", "drawn": "8", "lose": "3", "gd": "+38", "points": "59" },
+    { "position": "4.", "name": "Arsenal", "played": "28", "won": "17", "drawn": "3", "lose": "8", "gd": "+13", "points": "54" },
+    { "position": "5.", "name": "Tottenham Hotspur", "played": "29", "won": "16", "drawn": "3", "lose": "10", "gd": "+11", "points": "51" },
+    { "position": "6.", "name": "Manchester United", "played": "29", "won": "14", "drawn": "8", "lose": "7", "gd": "+8", "points": "50" },
+    { "position": "7.", "name": "West Ham United", "played": "30", "won": "14", "drawn": "6", "lose": "10", "gd": "+10", "points": "48" },
+    { "position": "8.", "name": "Wolverhampton Wanderers", "played": "30", "won": "14", "drawn": "4", "lose": "12", "gd": "+5", "points": "46" },
+    { "position": "9.", "name": "Aston Villa", "played": "29", "won": "11", "drawn": "3", "lose": "15", "gd": "+1", "points": "36" },
+    { "position": "10.", "name": "Leicster City", "played": "27", "won": "10", "drawn": "6", "lose": "11", "gd": "-4", "points": "36" },
+    { "position": "11.", "name": "Southampton", "played": "29", "won": "8", "drawn": "11", "lose": "10", "gd": "-9", "points": "35" },
+    { "position": "12.", "name": "Crystal Palace", "played": "29", "won": "7", "drawn": "13", "lose": "9", "gd": "+1", "points": "34" },
+    { "position": "13.", "name": "Brighton and Hove Albion", "played": "29", "won": "7", "drawn": "12", "lose": "10", "gd": "-10", "points": "33" },
+    { "position": "14.", "name": "Newcastle United", "played": "29", "won": "7", "drawn": "10", "lose": "12", "gd": "-17", "points": "31" },
+    { "position": "15.", "name": "Brentford", "played": "30", "won": "8", "drawn": "6", "lose": "16", "gd": "-14", "points": "30" },
+    { "position": "16.", "name": "Leeds United", "played": "30", "won": "7", "drawn": "8", "lose": "15", "gd": "-33", "points": "29" },
+    { "position": "17.", "name": "Everton", "played": "27", "won": "7", "drawn": "4", "lose": "16", "gd": "-18", "points": "25" },
+    { "position": "18.", "name": "Watford", "played": "29", "won": "6", "drawn": "4", "lose": "19", "gd": "-26", "points": "22" },
+    { "position": "19.", "name": "Burnley", "played": "27", "won": "3", "drawn": "12", "lose": "12", "gd": "-16", "points": "21" },
+    { "position": "20.", "name": "Norwich City", "played": "29", "won": "4", "drawn": "5", "lose": "20", "gd": "-45", "points": "17" },
+];
---
+++ backend/router.js
@@ -0,0 +1,11 @@
+// (GET) api/clubs
+
+
+//const databaseData = // database.js -> poziv na Firebase
+
+//const search(term) => {
+    // Machester City -> term: it
+    // SELECT * FROM klubovi WHERE name LIKE '%it%' // MySQL
+   // clubs.find(club => club.name.includes(term));
+//}
+
